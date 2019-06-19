40a625c376586d81a3761c3da4b060ad78e64d84
SS-C4 <ssc4@LAPTOP-DIL7Q993.localdomain>
2019-06-19 15:49:41 -0400

    add exhaustive search

--- code/likelihood_ratio.py
+++ code/likelihood_ratio.py
@@ -1,5 +1,9 @@
 from rumor_center import *
+from itertools import *
 import random
+
+def unique_perm(array):
+    return {p for p in permutations(array)}
 
 def to_int(adj):
     tmp = [[] for i in range(len(adj))]
@@ -21,13 +25,76 @@ nx.set_node_attributes(g1, g1_attr)
 g2 = nx.read_adjlist("g2_adj")
 
 ########################################
-#mapping of g2 -> g1
+#Stuff after the overlap has been set
 
-#Set g2_at to the exact overlap required. It is randomised right now, for purposes of testing.
+def stuff_after_overlap(g2_at):
+    g2_attr = ["a"+str(i) for i in g2_at]
+    g2_attr = {str(i) : {'label' : lab} for i,lab in enumerate(g2_attr)}
+
+    nx.set_node_attributes(g2, g2_attr)
+
+    #overlap -> true if labels are the same
+    #overlap is in terms of g1.nodes()
+    overlap = []
+    for i in g1.nodes():
+        for j in g2.nodes():
+            if g1.nodes[i]['label'] == g2.nodes[j]['label']:
+                overlap += [i]
+                break
+
+    #check if g1 and g2 are trees, if not, convert them to their BFS trees wrt node and find the max
+
+    if nx.is_tree(g1) and nx.is_tree(g2):    
+        #find rumor centers and RC values for all nodes
+        loc_1 = rumor_center(to_int(nx_graph_to_adj(g1)))
+        vals_g1 = RC_values(to_int(nx_graph_to_adj(g1)), loc_1)
+
+        vals_g1 = [(str(i),j) for (i,j) in vals_g1] 
+
+        loc_2 = rumor_center(to_int(nx_graph_to_adj(g2)))
+        vals_g2 = RC_values(to_int(nx_graph_to_adj(g2)), loc_2)
+
+        vals_g2 = [(str(i),j) for (i,j) in vals_g2]
+    else:
+        vals_g1 = [0 for i in range(g1.number_of_nodes())]
+        for i in g1.nodes():
+            h = (nx.bfs_tree(g1, i)).to_undirected()
+
+            loc = rumor_center(to_int(nx_graph_to_adj(h)))
+            tmpvals = RC_values(to_int(nx_graph_to_adj(h)), loc)
+            vals_g1[int(i)] = tmpvals[int(i)]
+
+        vals_g2 = [0 for i in range(g2.number_of_nodes())]
+        for i in g2.nodes():
+            h = (nx.bfs_tree(g2, i)).to_undirected()
+            
+            loc = rumor_center(to_int(nx_graph_to_adj(h)))
+            tmpvals = RC_values(to_int(nx_graph_to_adj(h)), loc)
+            vals_g2[int(i)] = tmpvals[int(i)]
+
+        vals_g1 = [(str(i),j) for (i,j) in vals_g1] 
+        vals_g2 = [(str(i),j) for (i,j) in vals_g2] 
+
+    #find num
+    num = 0
+    for i in overlap:
+        num += vals_g1[int(i)][1] * vals_g2[g2_at.index(int(i))][1]
+
+    #find denom
+    denom = sum([j for (i,j) in vals_g1])*sum([j for (i,j) in vals_g2]) - num
+
+    #ratio
+    return (float(num/denom))
+
+########################################
+#g2_at -> mapping of g2 -> g1
+#Set g2_at to the exact overlap required
 
 x = [i for i in range(6)]
 g2_at = [-1 for i in range(6)]
 
+'''
+#for randomised testing; ov -> size of overlap
 ov = 3
 for i in range(ov):
     r = random.randint(0,5)
@@ -39,69 +106,19 @@ for i in range(ov):
         s = random.randint(0,5)
     g2_at[s] = x[r]
 
-print (g2_at)
+stuff_after_overlap(g2_at)
+'''
 
-########################################
+#for exhaustive average of all possible overlaps of given size -> ov
+ov = 3
+replace_ov = list(combinations(x,ov))
 
-g2_attr = ["a"+str(i) for i in g2_at]
-g2_attr = {str(i) : {'label' : lab} for i,lab in enumerate(g2_attr)}
-
-nx.set_node_attributes(g2, g2_attr)
-
-#overlap -> true if labels are the same
-#overlap is in terms of g1.nodes()
-overlap = []
-for i in g1.nodes():
-    for j in g2.nodes():
-        if g1.nodes[i]['label'] == g2.nodes[j]['label']:
-            overlap += [i]
-            break
-
-#check if g1 and g2 are trees, if not, convert them to their BFS trees wrt node and find the max
-
-if nx.is_tree(g1) and nx.is_tree(g2):    
-    #find rumor centers and RC values for all nodes
-    loc_1 = rumor_center(to_int(nx_graph_to_adj(g1)))
-    vals_g1 = RC_values(to_int(nx_graph_to_adj(g1)), loc_1)
-
-    vals_g1 = [(str(i),j) for (i,j) in vals_g1] 
-
-    loc_2 = rumor_center(to_int(nx_graph_to_adj(g2)))
-    vals_g2 = RC_values(to_int(nx_graph_to_adj(g2)), loc_2)
-
-    vals_g2 = [(str(i),j) for (i,j) in vals_g2]
-else:
-    vals_g1 = [0 for i in range(g1.number_of_nodes())]
-    for i in g1.nodes():
-        h = (nx.bfs_tree(g1, i)).to_undirected()
-
-        loc = rumor_center(to_int(nx_graph_to_adj(h)))
-        tmpvals = RC_values(to_int(nx_graph_to_adj(h)), loc)
-        vals_g1[int(i)] = tmpvals[int(i)]
-
-    vals_g2 = [0 for i in range(g2.number_of_nodes())]
-    for i in g2.nodes():
-        h = (nx.bfs_tree(g2, i)).to_undirected()
+array = []
+for cc in replace_ov:
+    g2_at = list(cc)+g2_at[ov:]
+    for per in list(unique_perm(g2_at)):
+        array += [stuff_after_overlap(list(per))]
         
-        loc = rumor_center(to_int(nx_graph_to_adj(h)))
-        tmpvals = RC_values(to_int(nx_graph_to_adj(h)), loc)
-        vals_g2[int(i)] = tmpvals[int(i)]
-
-    vals_g1 = [(str(i),j) for (i,j) in vals_g1] 
-    vals_g2 = [(str(i),j) for (i,j) in vals_g2] 
-
-#find num
-num = 0
-for i in overlap:
-    num += vals_g1[int(i)][1] * vals_g2[g2_at.index(int(i))][1]
-print(num)
-
-#find denom
-denom = sum([j for (i,j) in vals_g1])*sum([j for (i,j) in vals_g2]) - num
-print(denom)
-
-#ratio
-print(float(num/denom))
-
+print (mean(array))
 
 
