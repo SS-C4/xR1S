89d869bcb205268fc35d9b7bb75665ef7b7335a4
SS-C4 <srirams@iisc.ac.in>
2019-06-22 20:24:02 -0400

    Format image with parameters

--- code/spread_exp_1.py
+++ code/spread_exp_1.py
@@ -8,34 +8,42 @@ from estimators import *
 
 
 #Restrict source to set R -> The first R nodes closest to the center
-R = 15
+R = 20
+#Stop after k infections in total (including source)
+k = 100
+#Number of estimators
+n_est = 5
+#Run experiments
+N = 4000
+#Number of divisions of [0,1] for threshold values
+n_div = 100
 
 def si_model_rumor_spreading(source, adjacency, N):
     infctn_pattern = [-1]*N;
     who_infected = [[] for i in range(N)]
-    
+
     # adding the source node to the list of infected nodes
     infctn_pattern[0] = source
     susceptible_nodes = adjacency[source]
     susceptible_indices = [0]*len(susceptible_nodes)
-        
+
     for i in range(1,N):
-        
+
         # infect the first node
         infctd_node_idx = rnd.randrange(0,len(susceptible_nodes),1)
         infctn_pattern[i] = susceptible_nodes[infctd_node_idx]
         who_infected[i] = [susceptible_indices[infctd_node_idx]]
         who_infected[susceptible_indices[infctd_node_idx]].append(i)
-        
+
         # updating susceptible_nodes and susceptible_indices
-        susceptible_indices = [susceptible_indices[j] for j in range(len(susceptible_nodes)) if susceptible_nodes[j] 
+        susceptible_indices = [susceptible_indices[j] for j in range(len(susceptible_nodes)) if susceptible_nodes[j]
                                != susceptible_nodes[infctd_node_idx]]
-        susceptible_nodes = [susceptible_nodes[j] for j in range(len(susceptible_nodes)) if susceptible_nodes[j] 
+        susceptible_nodes = [susceptible_nodes[j] for j in range(len(susceptible_nodes)) if susceptible_nodes[j]
                              != susceptible_nodes[infctd_node_idx]]
-        infctd_nodes = set(infctn_pattern[:i+1]) 
-        new_susceptible_nodes = set(adjacency[infctn_pattern[i]])                     
+        infctd_nodes = set(infctn_pattern[:i+1])
+        new_susceptible_nodes = set(adjacency[infctn_pattern[i]])
         new_susceptible_nodes = list(new_susceptible_nodes.difference(infctd_nodes))
-        susceptible_nodes  = susceptible_nodes  + new_susceptible_nodes 
+        susceptible_nodes  = susceptible_nodes  + new_susceptible_nodes
         susceptible_indices = susceptible_indices + [i]*len(new_susceptible_nodes)
 
     return who_infected, infctn_pattern
@@ -53,40 +61,29 @@ for i in range(1, R):
 	else:
 		liss += [z_nei[2] + i - 3]
 
-#Stop after k infections in total (including source)
-k = 100
-
 if k > G.number_of_nodes():
 	k = G.number_of_nodes()
 
-#1 & 1_1 are exp_1 
+#1 & 1_1 are exp_1
 #1 & 2 are exp_2
 #Less than threshold = diff
 #More than threshold = same
-
-#Number of estimators
-n_est = 5
-
-#Run experiments
-N = 2000
-
 #positive = same = More than threshold
 #negative = diff = Less than threshold
-tpr = [[] for i in range(n_est)] 
+tpr = [[] for i in range(n_est)]
 fpr = [[] for i in range(n_est)]
 
-for threshold in np.linspace(0,1,100):
+for threshold in np.linspace(0,1,n_div):
 	FP = [0 for i in range(n_est)]
 	TP = [0 for i in range(n_est)]
 	FN = [0 for i in range(n_est)]
 	TN = [0 for i in range(n_est)]
-	
+
 	op = [0 for i in range(n_est)]
 
 	for i in range(N):
 		#choose between exp 1 or exp 2
 		choice = rnd.randint(1,2)
-
 		if choice == 1:
 			source_1 = int(rnd.choice(liss))
 
@@ -95,7 +92,7 @@ for threshold in np.linspace(0,1,100):
 
 			s1 = G.subgraph([str(i) for i in inf_nodes_1])
 			s1_1 = G.subgraph([str(i) for i in inf_nodes_1_1])
-			
+
 			op[0] = est_0(G, s1, s1_1)
 			op[1] = est_1(G, s1, s1_1)
 			op[2] = est_2(G, s1, s1_1)
@@ -107,7 +104,7 @@ for threshold in np.linspace(0,1,100):
 					TP[i] += 1
 				elif op[i] < threshold:
 					FN[i] += 1
-			
+
 		else:
 			source_1 = int(rnd.choice(liss))
 			source_2 = int(rnd.choice(liss))
@@ -133,7 +130,7 @@ for threshold in np.linspace(0,1,100):
 	for i in range(n_est):
 		tpr[i] += [TP[i]/(TP[i]+FN[i])]
 		fpr[i] += [FP[i]/(TN[i]+FP[i])]
-	
+
 plt.plot([0,1], [0,1], 'b--')
 
 colors = ['g','r','c','m','y','k']
@@ -143,4 +140,5 @@ for i in range(n_est):
 	plt.plot(fpr[i], tpr[i], ''.join(colors[i] + markers[i]))
 
 plt.axis([0,1,0,1])
+plt.text(0.7, 0.4, "R: "+str(R) + "\nn_div: "+str(n_div) + "\nN: "+str(N) + "\nk: "+str(k))
 plt.show()
